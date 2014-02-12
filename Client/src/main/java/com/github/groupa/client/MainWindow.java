350e7a564a611930c87d5c79cde36c04f4e44ed6
Brynjulf <brynjulfz@hotmail.com>
2014-02-12 16:22:31 +0100

    Empty Frame of the GUI to add Panels on

--- Client/src/main/java/com/github/groupa/client/MainWindow.java
+++ Client/src/main/java/com/github/groupa/client/MainWindow.java
@@ -1,44 +1,47 @@
 package com.github.groupa.client;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
+/**
+ * Launch the application.
+ * 
+ * @author Brynjulf Risbakken
+ */
 public class MainWindow extends JFrame {
-
-	private JPanel contentPane;
+	private static final long serialVersionUID = 8268973863538368807L;
+	public static MainWindow mainWindow;
 
 	/**
-	 * Launch the application.
-	 * 
-	 * @author Brynjulf Risbakken
+	 * Create the frame.
 	 */
+	private MainWindow() {
+		JPanel content = new JPanel();
+		content.setOpaque(true);
+		setContentPane(content);
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		setPreferredSize(new Dimension(800, 600));
+		pack();
+		setVisible(true);
+	}
+	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					MainWindow frame = new MainWindow();
-					frame.setVisible(true);
+					mainWindow = new MainWindow();
+					
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	/**
-	 * Create the frame.
-	 */
-	public MainWindow() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
-	}
-
 }
