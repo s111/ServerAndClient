333b560a19d9156d2b0547802e9c6bb576045737
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-02-26 17:21:45 +0100

    Made sure that duplicated files does not get added to the database. The database is also cleared and filled after each reset.

--- Server/app/Global.java
+++ Server/app/Global.java
@@ -1,5 +1,12 @@
 import java.io.File;
 
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.dbplatform.H2Platform;
+import com.avaje.ebeaninternal.api.SpiEbeanServer;
+import com.avaje.ebeaninternal.server.ddl.DdlGenerator;
+
 import models.ImageModel;
 import play.Application;
 import play.GlobalSettings;
@@ -8,17 +15,37 @@ public class Global extends GlobalSettings {
 	@Override
 	public void onStart(Application application) {
 		File directory = new File("../../images");
-		
-		if (!directory.isDirectory()) return;
-		
+
+		if (!directory.isDirectory())
+			return;
+
 		File[] listOfFiles = directory.listFiles();
-		
+
 		for (File image : listOfFiles) {
 			String filename = image.getName();
-			
+
+			String filenameInDatabase = "../../images/" + filename;
+
 			if (filename.matches("^(.+).png$")) {
-				ImageModel.create("../../images/" + filename);
+				if (ImageModel.find.where().eq("filename", filenameInDatabase)
+						.findUnique() == null) {
+					ImageModel.create(filenameInDatabase);
+				}
+
 			}
 		}
+	}
+
+	@Override
+	public void onStop(Application app) {
+		String serverName = "default";
+
+		EbeanServer server = Ebean.getServer(serverName);
+		ServerConfig config = new ServerConfig();
+
+		DdlGenerator ddl = new DdlGenerator();
+		ddl.setup((SpiEbeanServer) server, new H2Platform(), config);
+		ddl.runScript(false, ddl.generateDropDdl());
+		ddl.runScript(false, ddl.generateCreateDdl());
 	}
 }
