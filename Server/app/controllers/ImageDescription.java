37f9f5429c236835d6a103e550e06bdeaec675b2
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-02 03:16:24 +0100

    Added route for adding a image description. Added the ImageDescription controller. Fixed the ImageRate test.

---
+++ Server/app/controllers/ImageDescription.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ImageDescription extends Controller {
+	public static Result setDescription(long id) {
+		return TODO;
+	}
+}
--- Server/test/controllers/ImageRaterTest.java
+++ Server/test/controllers/ImageRaterTest.java
@@ -4,10 +4,22 @@ import static org.junit.Assert.assertEquals;
 import static play.test.Helpers.callAction;
 import static play.test.Helpers.fakeApplication;
 import static play.test.Helpers.start;
+import static play.test.Helpers.stop;
+
+import java.io.IOException;
+
 import models.ImageModel;
 
+import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.Test;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.dbplatform.H2Platform;
+import com.avaje.ebeaninternal.api.SpiEbeanServer;
+import com.avaje.ebeaninternal.server.ddl.DdlGenerator;
 
 public class ImageRaterTest {
 	@Before
@@ -15,10 +27,33 @@ public class ImageRaterTest {
 		start(fakeApplication());
 	}
 	
+	@AfterClass
+	public static void stopApp() {
+		stop(fakeApplication());
+	}
+
+	@Before
+	public void resetDB() {
+		String serverName = "default";
+
+		EbeanServer server = Ebean.getServer(serverName);
+		ServerConfig config = new ServerConfig();
+
+		DdlGenerator ddl = new DdlGenerator();
+		ddl.setup((SpiEbeanServer) server, new H2Platform(), config);
+		ddl.runScript(false, ddl.generateDropDdl());
+		ddl.runScript(false, ddl.generateCreateDdl());
+	}
+
 	@Test
-	public void rate_image_1_with_rating_5_expect_rating_5() {
-		callAction(controllers.routes.ref.ImageRater.rate(1));
+	public void rate_image_1_with_default_rating_0_expect_rating_0() throws IOException {
+		String filename = "../../images/01.png";
 		
-		assertEquals(5, ImageModel.get(1).rating);
+		ImageModel createdImageModel = ImageModel.create(filename);
+		long id = createdImageModel.id;
+		
+		callAction(controllers.routes.ref.ImageRater.rate(id));
+
+		assertEquals(0, createdImageModel.rating);
 	}
 }
