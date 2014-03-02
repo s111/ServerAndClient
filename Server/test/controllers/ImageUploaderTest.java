5ac1e0b3ac97c13e43366e867c26979d02ea080b
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-02 16:33:29 +0100

    You are now able to upload images if you send a file to /api/image. This fixes #76. Also if you set limit to 0 in /api/images you will get back all the images on the server in one request

--- Server/test/controllers/ImageUploaderTest.java
+++ Server/test/controllers/ImageUploaderTest.java
@@ -1,5 +1,51 @@
 package controllers;
 
-public class ImageUploaderTest {
+import static org.junit.Assert.assertEquals;
+import static play.test.Helpers.callAction;
+import static play.test.Helpers.fakeApplication;
+import static play.test.Helpers.start;
+import static play.test.Helpers.stop;
+import models.ImageModel;
 
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.avaje.ebean.Ebean;
+import com.avaje.ebean.EbeanServer;
+import com.avaje.ebean.config.ServerConfig;
+import com.avaje.ebean.config.dbplatform.H2Platform;
+import com.avaje.ebeaninternal.api.SpiEbeanServer;
+import com.avaje.ebeaninternal.server.ddl.DdlGenerator;
+
+public class ImageUploaderTest {
+	@Before
+	public void startApp() {
+		start(fakeApplication());
+	}
+	
+	@AfterClass
+	public static void stopApp() {
+		stop(fakeApplication());
+	}
+
+	@Before
+	public void resetDB() {
+		String serverName = "default";
+
+		EbeanServer server = Ebean.getServer(serverName);
+		ServerConfig config = new ServerConfig();
+
+		DdlGenerator ddl = new DdlGenerator();
+		ddl.setup((SpiEbeanServer) server, new H2Platform(), config);
+		ddl.runScript(false, ddl.generateDropDdl());
+		ddl.runScript(false, ddl.generateCreateDdl());
+	}
+	
+	@Test
+	public void upload_image_without_value_expect_empty_list() {		
+		callAction(controllers.routes.ref.ImageUploader.upload());
+
+		assertEquals(0, ImageModel.getAll().size());
+	}
 }
