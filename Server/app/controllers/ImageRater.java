d9fe9adea875e62de850faeca4c9d25d4a9ef079
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-02 02:47:17 +0100

    Made /api/images/:id/rate use the post data with the key 'value' to specify the rating of the image. Fixes #44

--- Server/app/controllers/ImageRater.java
+++ Server/app/controllers/ImageRater.java
@@ -1,14 +1,29 @@
 package controllers;
 
 import models.ImageModel;
+import play.data.DynamicForm;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class ImageRater extends Controller {
 	public static Result rate(long id) {
+		DynamicForm requestData = Form.form().bindFromRequest();
+		
+		int rating = 0;
+		
+		try {
+			rating = Integer.parseInt(requestData.get("value"));
+		} catch (NumberFormatException exception) {
+			return badRequest();
+		}
+		
+		if (rating < 1 || rating > 5) return badRequest();
+		
+		
 		ImageModel imageModel = ImageModel.get(id);
 		
-		imageModel.rating = 5;
+		imageModel.rating = rating;
 		imageModel.save();
 		
 		return ok();
