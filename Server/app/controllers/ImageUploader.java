75415bc5fc2043a4ece269e0c7f9dbfec232cde9
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-02 15:33:18 +0100

    Added the /api/image route for image uploads. Added the controller and the associated test.

---
+++ Server/app/controllers/ImageUploader.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import play.mvc.Controller;
+import play.mvc.Result;
+
+public class ImageUploader extends Controller {
+	public static Result upload() {
+		return TODO;
+	}
+}
