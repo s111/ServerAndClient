25b6205fc1b9ec366184f8d8b589209bc8901908
Ragnvald Fuglestein <ragnvaldfuglestein@gmail.com>
2014-03-04 00:32:04 +0100

    Closed #88 Library instances can now specify some basic rules that control which pictures to include

--- Client/src/test/java/com/github/groupa/client/LibraryTest.java
+++ Client/src/test/java/com/github/groupa/client/LibraryTest.java
@@ -15,13 +15,13 @@ public class LibraryTest {
 		Library lib;
 		Requester requester = new FakeRequester();
 
-		assertEquals("Invalid count of images", Library.size(), 0);
+		assertEquals("Invalid count of images", 0, Library.size());
 
 		imgObject1 = Library.add(new ImageObject(1, requester));
 		lib = new Library();
 		imgObject2 = Library.add(new ImageObject(2, requester));
-		assertEquals("Invalid count of images", Library.size(), 2);
-		assertEquals("Invalid count of images", lib.imageCount(), 2);
+		assertEquals("Invalid count of images", 2, Library.size());
+		assertEquals("Invalid count of images", 2, lib.imageCount());
 
 		assertSame(imgObject1, lib.getImage());
 		assertSame(imgObject2, lib.getNextImage());
@@ -29,5 +29,26 @@ public class LibraryTest {
 		assertSame(imgObject2, lib.getPrevImage());
 		assertSame(imgObject1, lib.getPrevImage());
 		assertSame(imgObject2, lib.getPrevImage());
+		
+		lib.addConstraint(Library.ConstraintType.HAS_IMAGE);
+		assertEquals(0, lib.imageCount());
+		imgObject1.getImage();
+		imgObject2.getImage();
+		ImageObject imgObject3 = new ImageObject(3, requester);
+		Library.add(imgObject3);
+		assertEquals(2, lib.imageCount()); // Last image should be unavailable
+		lib.addConstraint(Library.ConstraintType.HAS_TAG,"tag");
+		assertEquals(0, lib.imageCount());
+		imgObject2.addTag("tag");
+		assertEquals(0, lib.imageCount());
+		lib.refresh();
+		assertEquals(1,lib.imageCount());
+		lib = new Library().addConstraint(Library.ConstraintType.HAS_IMAGE, true);
+		assertEquals(2, lib.imageCount());
+		lib.addConstraint(Library.ConstraintType.HAS_TAG, "tag");
+		assertEquals(1, lib.imageCount());
+		lib = new Library().addConstraint(Library.ConstraintType.HAS_IMAGE, false);
+		assertEquals(1, lib.imageCount());
+		// Will merge imageobject and library tests later.. This is just too messy.
 	}
 }
