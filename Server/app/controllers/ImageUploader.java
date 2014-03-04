d6c5c3cfe081c8e99efc2208f0726835c36759f9
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-04 11:24:50 +0100

    Fixed ImageUploader so that a image upload returns the correct 201 CREATED status code. Also changed the json returned to be equal a response from /api/images/:id on the image

--- Server/app/controllers/ImageUploader.java
+++ Server/app/controllers/ImageUploader.java
@@ -3,13 +3,16 @@ package controllers;
 import java.io.File;
 import java.io.IOException;
 
-import com.google.common.io.Files;
-
 import models.ImageModel;
+import play.libs.Json;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.google.common.io.Files;
 
 public class ImageUploader extends Controller {
 	public static final String IMAGE_DIRECTORY = "../../images/";
@@ -42,11 +45,61 @@ public class ImageUploader extends Controller {
 
 			ImageModel imageModel = ImageModel.create(IMAGE_DIRECTORY
 					+ filename);
+			
+			long id = imageModel.id;
 
-			return ok("{\"id\":" + imageModel.id + "}").as(
-					"application/json; charset=utf-8");
+			ObjectMapper mapper = new ObjectMapper();
+
+			ObjectNode rootNode = Json.newObject();
+			rootNode.put("href", routes.GetImage.info(id)
+					.absoluteURL(request()));
+
+			ImageModel nextImageModel = ImageModel.getNext(id);
+			ImageModel previousImageModel = ImageModel.getPrevious(id);
+			ImageModel firstImageModel = ImageModel.getFirst();
+			ImageModel lastImageModel = ImageModel.getLast();
+
+			long nextId = -1;
+			long previousId = -1;
+			long firstId = -1;
+			long lastId = -1;
+
+			if (nextImageModel != null) {
+				nextId = nextImageModel.id;
+			}
+
+			if (previousImageModel != null) {
+				previousId = previousImageModel.id;
+			}
+
+			if (firstImageModel != null) {
+				firstId = firstImageModel.id;
+			}
+
+			if (lastImageModel != null) {
+				lastId = lastImageModel.id;
+			}
+
+			rootNode.put("next", getAbsoluteURLToImageOrNull(nextId));
+
+			rootNode.put("previous", getAbsoluteURLToImageOrNull(previousId));
+
+			rootNode.put("first", getAbsoluteURLToImageOrNull(firstId));
+
+			rootNode.put("last", getAbsoluteURLToImageOrNull(lastId));
+
+			ObjectNode imageJSON = mapper.convertValue(imageModel, ObjectNode.class);
+
+			rootNode.put("image", imageJSON);
+
+			return created(rootNode);
 		} else {
 			return badRequest();
 		}
+	}
+	
+	private static String getAbsoluteURLToImageOrNull(long previousId) {
+		return previousId > 0 ? routes.GetImage.info(previousId)
+				.absoluteURL(request()) : null;
 	}
 }
