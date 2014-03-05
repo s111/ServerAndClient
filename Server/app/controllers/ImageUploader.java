b84fcff5f234a60cbe151fe6e06e8a4a166dbc4e
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-05 20:17:56 +0100

    Added support for uploading both jpgs and pngs

--- Server/app/controllers/ImageUploader.java
+++ Server/app/controllers/ImageUploader.java
@@ -1,9 +1,20 @@
 package controllers;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 
 import models.ImageModel;
+
+import org.apache.commons.io.FileUtils;
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+
+import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
@@ -30,12 +41,35 @@ public class ImageUploader extends Controller {
 			File file = image.getFile();
 			File newFile;
 
-			try {
-				newFile = File.createTempFile("image", ".png", new File(
-						IMAGE_DIRECTORY));
+			String extension = null;
 
-				Files.move(file, newFile);
-			} catch (IOException e) {
+			try {
+				extension = getExtensionFromMIME(file);
+			} catch (MimeTypeException exception) {
+				return handleMimeTypeExceptionWhileGettingExtension(file);
+			} catch (IOException exception) {
+				return handleIOExceptionWhileGettingExtension(file);
+			}
+
+			if (!extension.matches("\\.(jpg|png)"))
+				return badRequest();
+
+			File imageDirectory = new File(IMAGE_DIRECTORY);
+
+			if (!imageDirectory.exists()) {
+				imageDirectory.mkdir();
+			}
+
+			try {
+				newFile = File.createTempFile("image", extension,
+						imageDirectory);
+
+				Files.copy(file, newFile);
+
+				file.delete();
+			} catch (IOException exception) {
+				Logger.warn("IOException while trying to move file to image directory");
+
 				return internalServerError();
 			}
 
@@ -50,5 +84,35 @@ public class ImageUploader extends Controller {
 		} else {
 			return badRequest();
 		}
+	}
+
+	private static Result handleIOExceptionWhileGettingExtension(File file) {
+		Logger.warn("Failed to get extension from file due to IOException: "
+				+ file.getAbsolutePath());
+		Logger.warn("Can't continue with file upload");
+
+		return internalServerError();
+	}
+
+	private static Result handleMimeTypeExceptionWhileGettingExtension(File file) {
+		Logger.warn("Failed to get extension from file due to MimeTypeException: "
+				+ file.getAbsolutePath());
+		Logger.warn("Can't continue with file upload");
+
+		return internalServerError();
+	}
+
+	private static String getExtensionFromMIME(File file) throws IOException,
+			MimeTypeException {
+		TikaConfig config = TikaConfig.getDefaultConfig();
+
+		BufferedInputStream stream = new BufferedInputStream(
+				new FileInputStream(file));
+		MediaType mediaType = config.getMimeRepository().detect(stream,
+				new Metadata());
+		MimeType mimeType = config.getMimeRepository().forName(
+				mediaType.toString());
+
+		return mimeType.getExtension();
 	}
 }
