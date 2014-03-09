4bd90e198c651d3ef92e16ead0306469e9e7409d
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-09 14:01:28 +0100

    Created custom error handler for the rest client.

--- Client/src/main/java/com/github/groupa/client/App.java
+++ Client/src/main/java/com/github/groupa/client/App.java
@@ -1,9 +1,15 @@
 package com.github.groupa.client;
 
+import java.net.ConnectException;
+
 import javax.swing.JOptionPane;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
+
+import org.apache.log4j.BasicConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import retrofit.RestAdapter;
 
@@ -12,11 +18,16 @@ import com.github.groupa.client.jsonobjects.ImageShort;
 import com.github.groupa.client.servercommunication.RESTService;
 
 public class App {
+	private static final Logger logger = LoggerFactory
+			.getLogger(App.class);
+
 	private static String serverAPIBaseURL = "http://localhost:9000/api";
 
 	private static RESTService restService;
 
 	public static void main(String[] args) {
+		BasicConfigurator.configure();
+		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 		} catch (ClassNotFoundException | InstantiationException
@@ -26,8 +37,9 @@ public class App {
 		serverAPIBaseURL = JOptionPane.showInputDialog("Server API BaseURL",
 				serverAPIBaseURL);
 
-		RestAdapter restAdapter = new RestAdapter.Builder().setEndpoint(
-				serverAPIBaseURL).build();
+		RestAdapter restAdapter = new RestAdapter.Builder()
+				.setEndpoint(serverAPIBaseURL)
+				.setErrorHandler(new RESTErrorHandler()).build();
 
 		restService = restAdapter.create(RESTService.class);
 
@@ -45,7 +57,13 @@ public class App {
 	}
 
 	private static void getImages(String serverAPIBaseURL) {
-		ImageList imageList = restService.getImageList();
+		ImageList imageList = null;
+
+		try {
+			imageList = restService.getImageList();
+		} catch (ConnectException e) {
+			logger.error("Could not connect to the server: " + e.getMessage());
+		}
 
 		if (imageList == null)
 			return;
