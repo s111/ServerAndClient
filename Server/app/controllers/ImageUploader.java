bc0cdb365af556931f34ec54a6841ecd2cd21659
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-17 20:31:15 +0100

    Cleaned up ImageUploader controller. Put upload logic in a new Uploader class

--- Server/app/controllers/ImageUploader.java
+++ Server/app/controllers/ImageUploader.java
@@ -1,29 +1,14 @@
 package controllers;
 
-import java.io.BufferedInputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
 
-import json.generators.ImageInfoJsonGenerator;
-import models.ImageModel;
-import models.TagModel;
-
-import org.apache.tika.config.TikaConfig;
-import org.apache.tika.metadata.Metadata;
-import org.apache.tika.mime.MediaType;
-import org.apache.tika.mime.MimeType;
-import org.apache.tika.mime.MimeTypeException;
-
-import play.Logger;
 import play.mvc.Controller;
 import play.mvc.Http.MultipartFormData;
 import play.mvc.Http.MultipartFormData.FilePart;
 import play.mvc.Result;
-import url.generators.ImageInfoURLGenerator;
+import upload.Uploader;
 
 import com.fasterxml.jackson.databind.JsonNode;
-import com.google.common.io.Files;
 
 public class ImageUploader extends Controller {
 	public static final String IMAGE_DIRECTORY = "../../images/";
@@ -31,97 +16,34 @@ public class ImageUploader extends Controller {
 	public static Result upload() {
 		MultipartFormData body = request().body().asMultipartFormData();
 
-		FilePart image = null;
+		FilePart value = null;
 
 		if (body != null) {
-			image = body.getFile("value");
+			value = body.getFile("value");
 		} else {
 			return badRequest();
 		}
 
-		if (image != null) {
-			File file = image.getFile();
-			File newFile;
+		if (value != null) {
+			File image = value.getFile();
 
-			String extension = null;
+			Uploader uploader = new Uploader(image);
 
-			try {
-				extension = getExtensionFromMIME(file);
-			} catch (MimeTypeException exception) {
-				return handleMimeTypeExceptionWhileGettingExtension(file);
-			} catch (IOException exception) {
-				return handleIOExceptionWhileGettingExtension(file);
-			}
-
-			if (!extension.matches("\\.(jpg|png)"))
+			if (!uploader.hasValidExtension()) {
 				return badRequest();
-
-			File imageDirectory = new File(IMAGE_DIRECTORY);
-
-			if (!imageDirectory.exists()) {
-				imageDirectory.mkdir();
 			}
 
-			try {
-				newFile = File.createTempFile("image", extension,
-						imageDirectory);
+			boolean uploadSucceeded = uploader.upload();
 
-				Files.copy(file, newFile);
-
-				file.delete();
-			} catch (IOException exception) {
-				Logger.warn("IOException while trying to move file to image directory");
-
+			if (!uploadSucceeded) {
 				return internalServerError();
 			}
 
-			String filename = newFile.getName();
+			JsonNode imageInfoNode = uploader.toJson(request());
 
-			ImageModel imageModel = ImageModel.create(IMAGE_DIRECTORY
-					+ filename);
-			imageModel.addTag(TagModel.create("id:" + imageModel.id));
-
-			ImageInfoURLGenerator absoluteURLGenerator = new ImageInfoURLGenerator(
-					request());
-
-			ImageInfoJsonGenerator imageModelJsonGenerator = new ImageInfoJsonGenerator(
-					imageModel, absoluteURLGenerator);
-
-			JsonNode imageInfoNode = imageModelJsonGenerator.toJson();
-
-			return created(imageInfoNode);
+			return ok(imageInfoNode);
 		} else {
 			return badRequest();
 		}
-	}
-
-	private static Result handleIOExceptionWhileGettingExtension(File file) {
-		Logger.warn("Failed to get extension from file due to IOException: "
-				+ file.getAbsolutePath());
-		Logger.warn("Can't continue with file upload");
-
-		return internalServerError();
-	}
-
-	private static Result handleMimeTypeExceptionWhileGettingExtension(File file) {
-		Logger.warn("Failed to get extension from file due to MimeTypeException: "
-				+ file.getAbsolutePath());
-		Logger.warn("Can't continue with file upload");
-
-		return internalServerError();
-	}
-
-	private static String getExtensionFromMIME(File file) throws IOException,
-			MimeTypeException {
-		TikaConfig config = TikaConfig.getDefaultConfig();
-
-		BufferedInputStream stream = new BufferedInputStream(
-				new FileInputStream(file));
-		MediaType mediaType = config.getMimeRepository().detect(stream,
-				new Metadata());
-		MimeType mimeType = config.getMimeRepository().forName(
-				mediaType.toString());
-
-		return mimeType.getExtension();
 	}
 }
---
+++ Server/app/upload/Uploader.java
@@ -0,0 +1,160 @@
+package upload;
+
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import json.generators.ImageInfoJsonGenerator;
+import models.ImageModel;
+import models.TagModel;
+
+import org.apache.tika.config.TikaConfig;
+import org.apache.tika.metadata.Metadata;
+import org.apache.tika.mime.MediaType;
+import org.apache.tika.mime.MimeType;
+import org.apache.tika.mime.MimeTypeException;
+
+import play.Logger;
+import play.mvc.Http.Request;
+import url.generators.ImageInfoURLGenerator;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.google.common.io.Files;
+
+import controllers.ImageUploader;
+
+public class Uploader {
+	private File image;
+	private File newFile;
+	private File uploadDirectory;
+
+	private String extension;
+
+	private boolean uploaded;
+
+	public Uploader(File image) {
+		this.image = image;
+
+		trySettingExtension();
+	}
+
+	public boolean upload() {
+		makeSureUploadDirectoryExists();
+		boolean uploaded = tryTomoveTemporaryFileToUploadDirectory();
+
+		return uploaded;
+	}
+
+	public JsonNode toJson(Request request) {
+		if (!uploaded) {
+			throw new IllegalStateException(
+					"Cannot generate json if the upload did not succeed");
+		}
+
+		ImageModel imageModel = createImageModel();
+		JsonNode imageInfoNode = generateJson(request, imageModel);
+
+		return imageInfoNode;
+	}
+
+	private ImageModel createImageModel() {
+		String filename = newFile.getName();
+
+		ImageModel imageModel = ImageModel.create(ImageUploader.IMAGE_DIRECTORY
+				+ filename);
+		/* TODO Remove this before release */
+		imageModel.addTag(TagModel.create("id:" + imageModel.id));
+
+		return imageModel;
+	}
+
+	private JsonNode generateJson(Request request, ImageModel imageModel) {
+		ImageInfoURLGenerator absoluteURLGenerator = new ImageInfoURLGenerator(
+				request);
+
+		ImageInfoJsonGenerator imageModelJsonGenerator = new ImageInfoJsonGenerator(
+				imageModel, absoluteURLGenerator);
+
+		JsonNode imageInfoNode = imageModelJsonGenerator.toJson();
+
+		return imageInfoNode;
+	}
+
+	public boolean hasValidExtension() {
+		return extension.matches("\\.(jpg|png)");
+	}
+
+	private boolean tryTomoveTemporaryFileToUploadDirectory() {
+		try {
+			newFile = tryToCreateNewFile(newFile);
+
+			tryToCopyImageToNewFile(newFile);
+		} catch (IOException exception) {
+			return false;
+		} finally {
+			boolean imageIsDeleted = image.delete();
+
+			if (!imageIsDeleted) {
+				Logger.warn("Failed to delete temporary uploaded file");
+			}
+		}
+
+		return true;
+	}
+
+	private void tryToCopyImageToNewFile(File newFile) throws IOException {
+		try {
+			Files.copy(image, newFile);
+		} catch (IOException exception) {
+			Logger.error("Could not copy temporary image to upload directory");
+
+			throw exception;
+		}
+	}
+
+	private File tryToCreateNewFile(File newFile) throws IOException {
+		try {
+			newFile = File.createTempFile("image", extension, uploadDirectory);
+		} catch (IOException exception) {
+			Logger.error("Could not create new file");
+
+			throw exception;
+		}
+
+		return newFile;
+	}
+
+	private void makeSureUploadDirectoryExists() {
+		uploadDirectory = new File(ImageUploader.IMAGE_DIRECTORY);
+
+		if (!uploadDirectory.exists()) {
+			uploadDirectory.mkdir();
+		}
+	}
+
+	private void trySettingExtension() {
+		try {
+			this.extension = getExtension();
+		} catch (MimeTypeException e) {
+			Logger.error("Failed to get extension from file due to IOException: "
+					+ image.getAbsolutePath());
+		} catch (IOException e) {
+			Logger.warn("Failed to get extension from file due to MimeTypeException: "
+					+ image.getAbsolutePath());
+		}
+	}
+
+	private String getExtension() throws IOException, MimeTypeException {
+		TikaConfig config = TikaConfig.getDefaultConfig();
+
+		BufferedInputStream stream = new BufferedInputStream(
+				new FileInputStream(image));
+		MediaType mediaType = config.getMimeRepository().detect(stream,
+				new Metadata());
+		MimeType mimeType = config.getMimeRepository().forName(
+				mediaType.toString());
+
+		return mimeType.getExtension();
+	}
+}
