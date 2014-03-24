43507c68ca894f2ea0175b1df90e0a906e95faf9
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-03-24 13:27:43 +0100

    Removed the Welcome to Play welcome screen and replaced it with a listing of the images currently in the database.

--- Server/app/controllers/Application.java
+++ Server/app/controllers/Application.java
@@ -1,11 +1,21 @@
 package controllers;
 
+import java.util.List;
+
+import models.Image;
 import play.mvc.Controller;
 import play.mvc.Result;
+import queryDB.QueryImage;
+import utils.HibernateUtil;
 import views.html.index;
 
 public class Application extends Controller {
-    public static Result index() {
-        return ok(index.render("Your new application is ready."));
-    }
+	public static Result index() {
+		QueryImage queryImage = new QueryImage(
+				HibernateUtil.getSessionFactory());
+
+		List<Image> images = queryImage.getImages();
+
+		return ok(index.render(images));
+	}
 }
