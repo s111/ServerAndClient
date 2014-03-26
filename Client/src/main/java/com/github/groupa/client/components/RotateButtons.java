f76b6a9c46f372ff7f58a166a5f180988396c2cb
Jonas J. Husebø <jo-huseb@hotmail.com>
2014-03-26 16:11:45 +0100

    Added actionlisteners to the buttons. Closes #146

--- Client/src/main/java/com/github/groupa/client/components/RotateButtons.java
+++ Client/src/main/java/com/github/groupa/client/components/RotateButtons.java
@@ -1,6 +1,10 @@
 package com.github.groupa.client.components;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
+import javax.swing.JOptionPane;
 
 public class RotateButtons {
 	private JButton rotateCW;
@@ -9,7 +13,8 @@ public class RotateButtons {
 	public RotateButtons() {
 		rotateCW = new JButton("CW");
 		rotateCCW = new JButton("CCW");
-
+		
+		addActionListeners();
 	}
 
 	public JButton getRotateCWButton() {
@@ -19,4 +24,24 @@ public class RotateButtons {
 	public JButton getRotateCCWButton() {
 		return rotateCCW;
 	}
+	
+	
+	private void addActionListeners() {
+		rotateCW.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent action) {
+			 JOptionPane.showMessageDialog(null, "You clicked the rotate CW button!");
+			}
+		});
+		
+		rotateCCW.addActionListener(new ActionListener() {
+			
+			@Override
+			public void actionPerformed(ActionEvent action) {
+			JOptionPane.showMessageDialog(null, "You clicked the rotate CCW button!");
+			}
+		});
+	}
+
 }
