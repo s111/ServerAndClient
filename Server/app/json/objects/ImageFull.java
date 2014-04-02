d063b8b20d188832b8b1637aca0c207fb55149d1
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-04-02 19:04:17 +0200

    Removed no longer needed ImageFullList. Update ImageFull to reflect changes in clients ImageFull.

--- Server/app/json/objects/ImageFull.java
+++ Server/app/json/objects/ImageFull.java
@@ -6,44 +6,54 @@ import java.util.List;
 public class ImageFull {
 	private long id;
 
+	private List<Long> ids;
+
 	private int rating;
 
 	private String description;
 
-	private Timestamp date;
-
 	private List<String> tags;
+
+	private Timestamp date;
 
 	public long getId() {
 		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
 	}
 
 	public int getRating() {
 		return rating;
 	}
 
-	public void setRating(int rating) {
-		this.rating = rating;
-	}
-
 	public String getDescription() {
 		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
 	}
 
 	public List<String> getTags() {
 		return tags;
 	}
 
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
 	public void setTags(List<String> tags) {
 		this.tags = tags;
+	}
+
+	public List<Long> getIds() {
+		return ids;
+	}
+
+	public void setIds(List<Long> ids) {
+		this.ids = ids;
 	}
 
 	public Timestamp getDate() {
@@ -53,4 +63,5 @@ public class ImageFull {
 	public void setDate(Timestamp date) {
 		this.date = date;
 	}
+
 }
