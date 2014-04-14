1f56b2fa305d38355ca5d2e4891602436acc12a2
Sebastian Mæland Pedersen <sem.pedersen@stud.uis.no>
2014-04-14 15:30:34 +0200

    Removed unused imports.

--- Client/src/test/java/com/github/groupa/client/helpers/MockServerConnection.java
+++ Client/src/test/java/com/github/groupa/client/helpers/MockServerConnection.java
@@ -4,13 +4,6 @@ import static org.mockito.Mockito.mock;
 
 import com.github.groupa.client.servercommunication.ServerConnection;
 
-import java.awt.Image;
-import java.util.List;
-
-import com.github.groupa.client.ImageObject;
-import com.github.groupa.client.jsonobjects.ImageInfo;
-import com.github.groupa.client.servercommunication.ServerConnection;
-
 public class MockServerConnection {
 	public boolean returnValue = true;
 
